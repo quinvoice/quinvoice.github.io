52c7c024aa809347b992f4ceb3d0ed42111ae92a
Szymon Piecuch <szymek.piecuch@gmail.com>
2019-05-14 21:32:41 +0200

    First draft of landing page

---
+++ precache-manifest.3ad7f34770fa327bda228a3b71ea4db4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8271ab8943b5d56821d9570fda5115cc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2ccd8f4e87e69d5a1c50",
+    "url": "/static/css/main.c0d3fd26.chunk.css"
+  },
+  {
+    "revision": "279c20b8aac8847fca05",
+    "url": "/static/js/2.a611e342.chunk.js"
+  },
+  {
+    "revision": "2ccd8f4e87e69d5a1c50",
+    "url": "/static/js/main.91db42e6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
